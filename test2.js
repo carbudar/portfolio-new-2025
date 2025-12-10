dc94a529d87ee9ef5ef0f992c9d8326cbe602983
carbudar <carlaannabel26@gmail.com>
2025-12-10 15:37:39 -0500

    debugging

--- test2.js
+++ test2.js
@@ -135,7 +135,7 @@ if (projectKey === "Pinterest Case Study") {
 } else if (projectKey === "Stroll") {
     // Special case: static password-protected page
     moreInfoBtn.addEventListener('click', () => {
-        window.location.href = `testing.html`;
+        window.location.href = `stroll.html`;
     });
 } else{
     // Normal case: dynamic page with query param
